995d64e570dbd698e2f803d27188bfac19ac9100
Daniel Franco <danielf943015@gmail.com>
2023-01-20 16:57:00 -0500

    modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php 
 
-require_once 'home/appsalonmvcp/new2/includes/app.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/app.php';
 
 use Controllers\AdminController;
 use Controllers\APIController;
